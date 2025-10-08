bf3a7f0447c3536597def0750f15d86eea8c655d
printlndarling <printlndarling@qq.com>
2025-10-08 19:10:05 +0800

    Add adult video sources

--- js/config.js
+++ js/config.js
@@ -114,11 +114,60 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+    ckzy: {
+        api: 'https://www.ckzy1.com/api.php/provide/vod',
+        name: 'CK资源',
+        detail: 'https://www.ckzy1.com',
         adult: true
-    }
+    },
+    jkun: {
+        api: 'https://jkunzyapi.com/api.php/provide/vod',
+        name: 'jkun资源',
+        detail: 'https://jkunzyapi.com',
+        adult: true
+    },
+    bwzy: {
+        api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+        name: '百万资源',
+        detail: 'https://api.bwzym3u8.com',
+        adult: true
+    },
+    souav: {
+        api: 'https://api.souavzy.vip/api.php/provide/vod',
+        name: 'souav资源',
+        detail: 'https://api.souavzy.vip',
+        adult: true
+    },
+    r155: {
+        api: 'https://155api.com/api.php/provide/vod',
+        name: '155资源',
+        detail: 'https://155api.com',
+        adult: true
+    },
+    lsb: {
+        api: 'https://apilsbzy1.com/api.php/provide/vod',
+        name: 'lsb资源',
+        detail: 'https://apilsbzy1.com',
+        adult: true
+    },
+    huangcang: {
+        api: 'https://hsckzy.vip/api.php/provide/vod',
+        name: '黄色仓库',
+        detail: 'https://hsckzy.vip',
+        adult: true
+    },
+    yutu: {
+        api: 'https://yutuzy10.com/api.php/provide/vod',
+        name: '玉兔资源',
+        detail: 'https://yutuzy10.com',
+        adult: true
+    },
+    lebo: {
+        api: 'https://lbapi9.com/api.php/provide/vod',
+        name: 'AV-乐播资源',
+        detail: 'https://lbapi9.com',
+        adult: true
+  }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
